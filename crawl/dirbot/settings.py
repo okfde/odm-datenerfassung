1b70330be0c929cf373496c07b71319de27ecb53
Matt Fullerton <matt.fullerton@gmail.com>
2014-06-23 10:20:57 +0200

    Added a customized robotstxt which uses a different module so as to respect wildcards in robots.txt

--- crawl/dirbot/settings.py
+++ crawl/dirbot/settings.py
@@ -3,4 +3,8 @@
 SPIDER_MODULES = ['dirbot.spiders']
 NEWSPIDER_MODULE = 'dirbot.spiders'
 DEFAULT_ITEM_CLASS = 'dirbot.items.Website'
+SPIDER_MIDDLEWARES = {
+    'dirbot.middleware.robotstxt.RobotsTxtMiddleware': 543,
+}
+ROBOTSTXT_OBEY = True
 
