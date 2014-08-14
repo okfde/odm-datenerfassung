73d61868661d9d79340793d50a28a8be8ddabb43
Matt Fullerton <matt.fullerton@gmail.com>
2014-08-14 11:24:40 +0200

    Prioritization

--- utils/cleanDuplicates.py
+++ utils/cleanDuplicates.py
@@ -1,6 +1,8 @@
 import unicodecsv as csv
 import sys
 
+lowestulmparents = ('/metadaten/', '/single/')
+
 csvoutfile = open(sys.argv[2], 'wb')
 citywriter = csv.writer(csvoutfile, delimiter=',')
 cities = dict()
@@ -26,17 +28,23 @@ with open(sys.argv[1], 'rb') as csvfile:
     headings = next(cityreader, None)
     
     for inrow in cityreader:
-        cityurl = inrow[0]
-        searchurl = inrow[1]
+        try:
+            cityurl = inrow[0]
+            searchurl = inrow[1]
+            parenturl = inrow[6]
+        except:
+            print u'Data format error: ' + u', '.join(inrow) + u'\nStopping.'
         found = False
 
         print "Searching for " + searchurl
 
-        #Prioritize records with all data
-        #In general this shouldn't happen, as the file is downloaded
-        #after the link is found
         if searchurl in cities[cityurl]:
-            if ('ermittelt' in cities[cityurl][searchurl][6]) and ('ermittelt' not in inrow[6]):
+            #Prioritize records with all data
+            #In general this shouldn't happen, as the file is downloaded
+            #after the link is found
+            if ((('ermittelt' in cities[cityurl][searchurl][6]) and ('ermittelt' not in parenturl)) or 
+            #Ulm special handling to prevent using links from summary pages
+            ((cityurl == 'ulm.de') and any(x in parenturl for x in lowestulmparents))):
                 cities[cityurl][searchurl] = inrow
                 print "Replacing row"
             else:
