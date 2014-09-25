ba3f8b251cbefb43668bfdbd286be09560eece35
Matt Fullerton <matt.fullerton@gmail.com>
2014-09-25 13:37:31 +0200

    Make crawler more robust/sustainable: - Detect repeating URL paths/arguments - Allow blacklist items per domain (taken from DB) or globally

--- crawl/dirbot/middleware/robotstxt.py
+++ crawl/dirbot/middleware/robotstxt.py
@@ -3,6 +3,7 @@ This is a middleware to respect robots.txt policies. To activate it you must
 enable this middleware and enable the ROBOTSTXT_OBEY setting.
 
 """
+import re
 
 from reppy.cache import RobotsCache
 
@@ -19,13 +20,19 @@ class RobotsTxtMiddleware(object):
         if not crawler.settings.getbool('ROBOTSTXT_OBEY'):
             raise NotConfigured
 
-        self.blacklist = crawler.settings.get('ROBOTSTXT_BLACKLIST', ())
+        self.completeblacklist = crawler.settings.get('ROBOTSTXT_BLACKLIST', ())
+        self.blacklist = []
+        self.generalblacklist = crawler.settings.get('GENERAL_BLACKLIST', ())
+        self.hasblacklist = False
         self.whitelist = crawler.settings.get('ROBOTSTXT_WHITELIST', ())
         self.crawler = crawler
         self._useragent = crawler.settings.get('USER_AGENT')
         self._parsers = {}
         self._spider_netlocs = set()
         self.robots = RobotsCache()
+        
+        self.stoprepetitionsrearg = re.compile(ur'.*?\&(.*?\&)\1{1,}.*')
+        self.stoprepetitionsreslash = re.compile(ur'.*?\/(.*?\/)\1{1,}.*')
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -33,8 +40,23 @@ class RobotsTxtMiddleware(object):
 
     def process_request(self, request, spider):
         useragent = self._useragent
+        if not self.hasblacklist:
+            self.hasblacklist = True
+            if ('http://' + spider.domain) in self.completeblacklist and self.completeblacklist['http://' + spider.domain] != None:
+                self.blacklist = [el.lower() for el in self.completeblacklist['http://' + spider.domain]]
+                log.msg(format="Got blacklist from DB for domain",
+                    level=log.DEBUG, request=request)
+            else:
+                log.msg(format="Didn't get a blacklist from DB for domain",
+                    level=log.DEBUG, request=request)
+            self.blacklist.extend([el.lower() for el in self.generalblacklist])
+        #Check for silly repeating arguments
+        if self.stoprepetitionsrearg.match(request.url) != None or self.stoprepetitionsreslash.match(request.url) != None:
+            log.msg(format="URL is suspicious: %(request)s",
+                    level=log.DEBUG, request=request)
+            raise IgnoreRequest
         #Blacklist overrides whitelist and robots
-        if any(bl in request.url for bl in self.blacklist):
+        if any(bl in request.url.lower() for bl in self.blacklist):
             log.msg(format="Forbidden by blacklist: %(request)s",
                     level=log.DEBUG, request=request)
             raise IgnoreRequest
--- crawl/dirbot/settings.py
+++ crawl/dirbot/settings.py
@@ -1,5 +1,7 @@
 # Scrapy settings for dirbot project
 
+import psycopg2
+
 SPIDER_MODULES = ['dirbot.spiders']
 NEWSPIDER_MODULE = 'dirbot.spiders'
 DEFAULT_ITEM_CLASS = 'dirbot.items.Website'
@@ -8,7 +10,9 @@ DOWNLOADER_MIDDLEWARES = {
     'scrapy.contrib.downloadermiddleware.robotstxt.RobotsTxtMiddleware': None,
 }
 ROBOTSTXT_OBEY = True
-ROBOTSTXT_BLACKLIST = ('urban.gera.de', 'sitzungsdienst.salzgitter.de', 'ris.kaiserslautern.de','extranet.iserlohn.de', 'salzgitter.de/tourismus/events/index.php', 'timestamp=', 'textmodus=&textmodu', 'modul=druckansicht', 'switchtodate', 'dienstleistungen.php', 'siegen.de/vereinsregister', 'mitarbeiter/mitarbeiter.php', 'siegen.de/veranstaltungen', 'dienstleistungen/formular.php', 'events/list.php', 'lexikon/index.php', '.krebs/karte', 'cottbus.de/opt', 'cottbus.de/abfrage', 'sixcms/%20/sixcms', 'sixcms/sixcms', 'search', 'suche', 'dataset?f', 'branchen', 'mobil.koeln.de', 'flag_content', 'comment', 'immobilien.koeln', 'stadtfuehrungen', 'stadtplan.html', 'veranstaltungen/kalender', 'koeln.de/kleinanzeigen', '/feedback/' , '/recommend/', 'termine.koeln', 'bildung.koeln', 'anwendungen.bielefeld', 'rostock.de/veranstaltungen', 'wahlomat', 'php/merkliste', '_druck=1', 'unt_tagung', 'buergerinfo.ulm.de', 'map.jsp')
+GENERAL_BLACKLIST = ('veranstaltungskalender', 'veranstaltungen', 'font=', 'print=', 'style=', 'font_size=', 'lang=')
+#Old - not deleting as this needs to be transferred to the DB as far as possible
+#ROBOTSTXT_BLACKLIST = ('urban.gera.de', 'sitzungsdienst.salzgitter.de', 'ris.kaiserslautern.de','extranet.iserlohn.de', 'salzgitter.de/tourismus/events/index.php', 'timestamp=', 'textmodus=&textmodu', 'modul=druckansicht', 'switchtodate', 'dienstleistungen.php', 'siegen.de/vereinsregister', 'mitarbeiter/mitarbeiter.php', 'dienstleistungen/formular.php', 'events/list.php', 'lexikon/index.php', '.krebs/karte', 'cottbus.de/opt', 'cottbus.de/abfrage', 'sixcms/%20/sixcms', 'sixcms/sixcms', 'search', 'suche', 'dataset?f', 'branchen', 'mobil.koeln.de', 'flag_content', 'comment', 'immobilien.koeln', 'stadtfuehrungen', 'stadtplan.html', 'koeln.de/kleinanzeigen', '/feedback/' , '/recommend/', 'termine.koeln', 'bildung.koeln', 'anwendungen.bielefeld', 'wahlomat', 'php/merkliste', '_druck=1', 'unt_tagung', 'buergerinfo.ulm.de', 'map.jsp')
 ROBOTSTXT_WHITELIST = ('/wahlen')
 AUTOTHROTTLE_ENABLED = True
 AUTOTHROTTLE_START_DELAY = 3.0
@@ -17,3 +21,21 @@ DEPTH_PRIORITY = 1
 SCHEDULER_DISK_QUEUE = 'scrapy.squeue.PickleFifoDiskQueue'
 SCHEDULER_MEMORY_QUEUE = 'scrapy.squeue.FifoMemoryQueue'
 USER_AGENT = "Open_Data_Crawler/0.1 (+http://open-data-map.de)"
+
+ROBOTSTXT_BLACKLIST = dict()
+
+con = None
+    
+try:
+    con = psycopg2.connect(database='odm', user='postgres', password='p0stgre5', host='127.0.0.1')
+    cur = con.cursor()
+    cur.execute("SELECT url, crawl_blacklist FROM cities;")
+    results = cur.fetchall()
+    for result in results:
+        ROBOTSTXT_BLACKLIST[result[0]] = result[1] #Key is city url, value is array of forbidden URL parts    
+except psycopg2.DatabaseError, e:
+    print 'Database error: %s' % e
+    exit()
+finally:
+    if con:
+        con.close()
