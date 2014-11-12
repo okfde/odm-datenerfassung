3d6152aa8022da0e3b84b1f1f1ada654426b43bb
Matt Fullerton <matt.fullerton@gmail.com>
2014-11-12 08:46:42 +0100

    More efficient parent searching

--- utils/cleanDuplicates.py
+++ utils/cleanDuplicates.py
@@ -1,6 +1,9 @@
 import unicodecsv as csv
 import sys
 
+def simplifyurl(url):
+    return u'/'.join(url.split('/')[3:])
+
 if (len(sys.argv)<3):
     print 'Usage: cleanDuplicates filetoclean.csv outputfile.csv [supportingitemsfulllist.csv]'
     exit()
@@ -63,31 +66,59 @@ if (len(sys.argv)>3):
     findparentsource = True
     parentsfile = sys.argv[3]
 
+missinglinks = {}
+
 for city in cities.values():
     for outputrow in city.values():
-        if 'ermittelt' in outputrow[6] and findparentsource:
-            #These links always have http etc. in them, the stored links from the A elements may not
-            missinglink = u'/'.join(outputrow[1].split('/')[3:])
-            print 'Searching for link component: ' + missinglink + ' in entire items file'
-            #Unfortunately it won't work if the URL arguments get rearranged :(
-            #If it becomes a problem we should parse the URLs
-            with open(sys.argv[3], 'rb') as csvfile:
-                parentreader = csv.reader(csvfile, delimiter=',')
-                #Skip headings
-                headings = next(parentreader, None)
+        try:
+            if 'ermittelt' in outputrow[6] and findparentsource:
+                #These links always have http etc. in them, the stored links from the A elements may not
+                missinglink = simplifyurl(outputrow[1])
+                print 'Will search for link component: ' + missinglink + ' in entire items file'
+                missinglinks[missinglink] = {}
+                missinglinks[missinglink]['actualurl'] = outputrow[1]
+                missinglinks[missinglink]['parenturl'] = 'Nicht gefunden'
+                missinglinks[missinglink]['parenttitle'] = 'Nicht gefunden'
+        except:
+            print outputrow
 
-                for inrow in parentreader:
-                    try:
-                        fileurl = inrow[1]
-                    except:
-                        print inrow
-                        fileurl = ''
-                    if missinglink in fileurl and 'ermittelt' not in inrow[6]:
-                        print 'FOUND'
-                        outputrow[2] = inrow[2]
-                        outputrow[6] = inrow[6]
-                        outputrow[7] = inrow[7]      
-                        break
+if findparentsource:
+    #Unfortunately it won't work if the URL arguments get rearranged :(
+    #If it becomes a problem we should parse the URLs
+    with open(sys.argv[3], 'rb') as csvfile:
+        parentreader = csv.reader(csvfile, delimiter=',')
+        #Skip headings
+        headings = next(parentreader, None)
+
+        count = 0
+        
+        for inrow in parentreader:
+            count += 1
+            if count % 100 == 0:
+                print str(count) + ' rows read'
+            try:
+                if 'ermittelt' not in inrow[6]:
+                    linkurl = inrow[1] #URL of link found. May or may not contain http
+                    potentialparent = inrow[6]
+                    for mlkey in missinglinks.keys():
+                        if mlkey in linkurl:
+                            print 'Found a parent: ' + potentialparent + ' for ' + mlkey
+                            missinglinks[mlkey]['actualurl'] = inrow[2]
+                            missinglinks[mlkey]['parenturl'] = inrow[6]
+                            missinglinks[mlkey]['parenttitle'] = inrow[7]
+            except:
+                print inrow
+                
+for city in cities.values():
+    for outputrow in city.values():
+        try:
+            if 'ermittelt' in outputrow[6] and findparentsource:
+                urlkey = simplifyurl(outputrow[1])
+                outputrow[2] = missinglinks[urlkey]['actualurl']
+                outputrow[6] = missinglinks[urlkey]['parenturl']
+                outputrow[7] = missinglinks[urlkey]['parenttitle']
+        except:
+            print outputrow
         citywriter.writerow(outputrow)
 
 csvfile.close();
