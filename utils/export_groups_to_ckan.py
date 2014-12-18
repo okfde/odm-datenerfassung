fc2485f7e98570da3047f9878abe28795c8a06d3
Sebastian Berchtold <Seb.Berchtold@web.de>
2014-12-18 09:35:57 +0100

    Add group ids
    Since ckan harvester requires ids to match groups.

--- utils/export_groups_to_ckan.py
+++ utils/export_groups_to_ckan.py
@@ -13,6 +13,7 @@ apikey = ''
 def create_group(name):
     dataset_dict = {}
     dataset_dict['title'] = name
+    dataset_dict['id'] = metautils.force_alphanumeric_short(name)
     dataset_dict['name'] = metautils.force_alphanumeric_short(name)
     data_string = urllib.quote(json.dumps(dataset_dict))
     request = urllib2.Request(url + '/api/3/action/group_create')
